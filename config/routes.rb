bfc5ddd2099f827e70b09a0e3367af68b4a3d7ff
Tensae Mamo <tensaemamo@protonmail.com>
2024-01-22 15:09:04 +0100

    added /score route and action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "new", to:"games#new"
+  get "score", to:"games#score"
   # Defines the root path route ("/")
   # root "posts#index"
 end
